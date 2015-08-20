ac69ba63540da6e258f8a23f9d4ac17ae27aa4d0
Sebastian Freundt <freundt@ga-group.nl>
2015-08-20 05:24:41 +0000

    minor, provide proper open()ing semantics regardless if it's a filename, -, or NULL

--- src/xsltmultiproc.c
+++ src/xsltmultiproc.c
@@ -76,6 +76,19 @@ error(const char *fmt, ...)
 	return errno;
 }
 
+static int
+xopen(const char *fn, int fl)
+{
+	int fd;
+
+	if (fn == NULL || *fn == '-' && fn[1U] == '\0') {
+		fd = STDIN_FILENO;
+	} else {
+		fd = open(fn, fl);
+	}
+	return fd;
+}
+
 static char*
 xmemmem(const char *hay, const size_t hayz, const char *ndl, const size_t ndlz)
 {
@@ -218,9 +231,7 @@ proc1(xsltStylesheetPtr sty, const char *fn)
 	int rc = 0;
 	int fd;
 
-	if (fn == NULL || *fn == '-' && fn[1U] == '\0') {
-		fd = STDIN_FILENO;
-	} else if ((fd = open(fn, O_RDONLY)) < 0) {
+	if ((fd = xopen(fn, O_RDONLY)) < 0) {
 		rc = -1;
 		goto out;
 	}
